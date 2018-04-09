f0a3d4d36acc794fd58f5e4ee858b2d7462a2c53
VirajDeshwal <viraj.deshwal@outlook.com>
2018-04-09 13:52:13 -0700

    model configured
    input shape in top model is configured

--- samplenet/network.py
+++ samplenet/network.py
@@ -2,13 +2,17 @@
 from __future__ import print_function
 from keras import layers, Input
 from keras.models import Model
+from keras.applications import ResNet50
+from keras.layers import  Flatten, Dense, Dropout, Input
 
 class Network(object):
     def __init__(self, **kwargs): 
         pass
     
     def get_network(self):
+        base_model = ResNet50(weights='imagenet', include_top=False)
         input_tensor = Input(shape=(448, 448, 1, ))
+        model = base_model(input_tensor)
         x = layers.Conv2D(32, kernel_size=(3, 3), activation='relu')(input_tensor)
         x = layers.MaxPooling2D(pool_size=(3, 3))(x)
         x = layers.Conv2D(64, kernel_size=(3, 3), activation='relu')(x)
@@ -21,4 +25,4 @@ class Network(object):
         x = layers.Dropout(0.5)(x)
         output_tensor = layers.Dense(128, activation="softmax")(x)
         model  = Model(input_tensor, output_tensor)
-        return model
+        print( model.summary())
