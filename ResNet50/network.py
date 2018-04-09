cf176a50d7a159fc1e455b6ea3efbdf1c836ba5d
Anushree Ankola <anushreeankola@csu.fullerton.edu>
2018-04-09 08:29:03 -0700

    ResNet50 model-AnushreeAnkola

--- ResNet50/network.py
+++ ResNet50/network.py
@@ -2,17 +2,15 @@
 from __future__ import print_function
 from keras import layers, Input
 from keras.models import Model
-from keras.applications.xception import Xception
+from keras.applications import ResNet50
 
 class Network(object):
     def __init__(self, kwargs):
         self.unfreeze_layers = kwargs.unfreeze_layers
     
     def get_network(self):
-        model = Xception(include_top=False, input_shape=(448, 448, 3))
+        model = ResNet50(include_top=False, input_shape=(448, 448, 3))
         x = model.output
-        x = layers.GlobalAveragePooling2D()(x)
-        x = layers.Dense(512, activation='relu')(x)
         x = layers.Dropout(0.5)(x)
         output = layers.Dense(128, activation='softmax', name='output')(x)
         return Model(model.input, output)
