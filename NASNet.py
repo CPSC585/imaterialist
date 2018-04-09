63d33d703725f983f8f04868346e4ef22abae21d
Himanshu Raj <rhimanshu909@gmail.com>
2018-04-09 15:09:42 -0700

    Update NASNet.py

--- NASNet.py
+++ NASNet.py
@@ -9,6 +9,7 @@ Created on Mon Apr  9 14:25:55 2018
 from __future__ import print_function
 from keras import layers, Input
 from keras.models import Model
+from keras.applications import NASNetLarge
 
 class Network(object):
     def __init__(self, **kwargs): 
@@ -16,8 +17,9 @@ class Network(object):
     
     def get_network(self):
         my_model = NASNetLarge(include_top=False, weights='imagenet')
-        input_tensor = Input(shape=(448, 448, 1, ))
-        x = layers.Conv2D(32, kernel_size=(3, 3), activation='relu')(input_tensor)
+        input_tensor = Input(shape=(448, 448, 3))
+        output_nasnet = my_model(input_tensor)
+        x = layers.Conv2D(32, kernel_size=(3, 3), activation='relu')(output_nasnet)
         x = layers.MaxPooling2D(pool_size=(3, 3))(x)
         x = layers.Conv2D(64, kernel_size=(3, 3), activation='relu')(x)
         x = layers.MaxPooling2D(pool_size=(3, 3))(x)
@@ -28,5 +30,5 @@ class Network(object):
         x = layers.Dense(128, activation='relu')(x)
         x = layers.Dropout(0.5)(x)
         output_tensor = layers.Dense(128, activation="softmax")(x)
-        model  = Model(input_tensor, output_tensor)
+        model  = Model(input_tensor, output_tensor)        
         return model
