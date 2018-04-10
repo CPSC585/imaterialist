7220a3f5764943b1843799fa997d78ef206dc8fe
NanBai <bnking1988@gmail.com>
2018-04-09 17:42:49 -0700

    VGG16
    add another version which already integrated in keras

---
+++ VGG16/network_v2.py
@@ -0,0 +1,19 @@
+# This is a template for sample network
+from __future__ import print_function
+from keras import layers, Input
+from keras.models import Model
+from keras.applications.vgg16 import VGG16
+
+class Network(object):
+    def __init__(self, **kwargs): 
+        pass
+    
+    def get_network(self):
+        model = VGG16(include_top=False, weights='imagenet', input_shape=(224, 224, 3), pooling='avg')
+        x = model.output
+        x = layers.Dense(4096, activation='relu')(x)
+        x = layers.Dropout(0.5)(x)
+        output_tensor = layers.Dense(1000, activation="softmax")(x)
+            
+        model  = Model(model.input, output_tensor)
+        return model
