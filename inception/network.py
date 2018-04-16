b2b8571c200891526f35b51ee4512a81e648afc0
Akatama <jimmyl@pacbell.net>
2018-04-16 16:18:05 -0700

    Changes to the code that the professor requested

--- inception/network.py
+++ inception/network.py
@@ -1,4 +1,4 @@
-# This is a template for sample network
+# This is an implementation of InceptionV3
 from __future__ import print_function
 import keras
 from keras import layers, Input
@@ -10,17 +10,25 @@ class Network(object):
     def __init__(self, **kwargs): 
         pass
     
-    def get_network(self, unfreezeLayers):
-        modelV3 = inception_v3.InceptionV3(include_top=False, weights='imagenet',
-                                           input_shape=(448, 448, 3), classes=128)
+    def get_network(self, options):
+    	if options.pretrained:
+        	modelV3 = inception_v3.InceptionV3(include_top=False, weights='imagenet',
+                                           input_shape=(448, 448, 3))
+        else:
+        	modelV3 = inception_v3.InceptionV3(include_top=False,
+                                           input_shape=(448, 448, 3))
         x = modelV3.output
         x = GlobalAveragePooling2D(name='avg_pool')(x)
         output_tensor = Dense(128, activation='softmax' name="predictions")(x)
-        model  = Model(modelV3.input, output_tensor)
         
-        if len(unfreezeLayers) if > 0:
+
+        if options.freeze_layers:
             for layer in modelV3.layers:
-                if not layer.name in unfreezeLayers:
-                    layer.trainable = False
-        
+            	if any(map(layer.name.startwith, options.unfreeze_layers)):
+            		layer.trainable = True
+            	else:
+            		layer.trainable = False
+
+
+        model = Model(modelV3.input, output_tensor)
         return model
