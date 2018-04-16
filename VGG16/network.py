e5fe76021e42252d66e7612f1468a33c9444a9d2
NanBai <bnking1988@gmail.com>
2018-04-16 15:25:28 -0700

    fixed

--- VGG16/network.py
+++ VGG16/network.py
@@ -1,60 +1,28 @@
-# This is a template for sample network
 from __future__ import print_function
 from keras import layers, Input
 from keras.models import Model
+from keras.applications.vgg16 import VGG16
 
 class Network(object):
-    def __init__(self, **kwargs): 
-        pass
+    def __init__(self, kwargs):
+        self.unfreeze_layers = kwargs.unfreeze_layers
     
-    def get_network(self):
-        input_tensor = Input(shape=(224, 224, 3, ))
-        x = layers.ZeroPadding2D((1,1),input_tensor)
-        x = layers.Conv2D(64, kernel_size=(3, 3), activation='relu')(x)
-        x = layers.ZeroPadding2D((1,1))(x)
-        x = layers.Conv2D(64, kernel_size=(3, 3), activation='relu')(x)
-        x = layers.MaxPooling2D(pool_size=(2, 2), strides=(2,2))(x)
+    def get_network(self, options):
+        if options.pretrained:
+            model = VGG16(include_top=False, weights='imagenet', input_shape=(448, 448, 3))
+        else:
+            model = VGG16(include_top=False, input_shape=(448, 448, 3))
         
-        x = layers.ZeroPadding2D((1,1))(x)
-        x = layers.Conv2D(128, kernel_size=(3, 3), activation='relu')(x)
-        x = layers.ZeroPadding2D((1,1))(x)
-        x = layers.Conv2D(128, kernel_size=(3, 3), activation='relu')(x)
-        x = layers.MaxPooling2D(pool_size=(2, 2), strides=(2,2))(x)
-        
-        x = layers.ZeroPadding2D((1,1))(x)
-        x = layers.Conv2D(256, kernel_size=(3, 3), activation='relu')(x)
-        x = layers.ZeroPadding2D((1,1))(x)
-        x = layers.Conv2D(256, kernel_size=(3, 3), activation='relu')(x)
-        x = layers.ZeroPadding2D((1,1))(x)
-        x = layers.Conv2D(256, kernel_size=(3, 3), activation='relu')(x)
-        x = layers.MaxPooling2D(pool_size=(2, 2), strides=(2,2))(x)
-        
-        x = layers.ZeroPadding2D((1,1))(x)
-        x = layers.Conv2D(512, kernel_size=(3, 3), activation='relu')(x)
-        x = layers.ZeroPadding2D((1,1))(x)
-        x = layers.Conv2D(512, kernel_size=(3, 3), activation='relu')(x)
-        x = layers.ZeroPadding2D((1,1))(x)
-        x = layers.Conv2D(512, kernel_size=(3, 3), activation='relu')(x)
-        x = layers.MaxPooling2D(pool_size=(2, 2), strides=(2,2))(x)
-        
-        x = layers.ZeroPadding2D((1,1))(x)
-        x = layers.Conv2D(512, kernel_size=(3, 3), activation='relu')(x)
-        x = layers.ZeroPadding2D((1,1))(x)
-        x = layers.Conv2D(512, kernel_size=(3, 3), activation='relu')(x)
-        x = layers.ZeroPadding2D((1,1))(x)
-        x = layers.Conv2D(512, kernel_size=(3, 3), activation='relu')(x)
-        x = layers.MaxPooling2D(pool_size=(2, 2), strides=(2,2))(x)
-        
-        x = layers.Flatten()(x)
+        x = model.output
         x = layers.Dense(4096, activation='relu')(x)
         x = layers.Dropout(0.5)(x)
-        x = layers.Dense(4096, activation='relu')(x)
-        x = layers.Dropout(0.5)(x)
-        output_tensor = layers.Dense(1000, activation="softmax")(x)
+        output_tensor = layers.Dense(128, activation="softmax")(x)
         
-#        if weights_path:
-#            model.load_weights(weights_path)
-#            return model
-            
-        model  = Model(input_tensor, output_tensor)
-        return model
+        if options.freeze_layers:
+                    for layer in model.layers:
+                        if any(map(layer.name.startswith, options.unfreeze_layers)):
+                            layer.trainable = True
+                        else:
+                            layer.trainable = False
+                            
+        return Model(model.input, output_tensor)
