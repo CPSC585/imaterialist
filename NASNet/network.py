8d11a9d10d2a20babdcd06d96e6add5a1ecec1cb
Rishabh Sharma <rishabh230795@gmail.com>
2018-04-23 16:13:48 -0700

    Modified NASNet

--- NASNet/network.py
+++ NASNet/network.py
@@ -8,19 +8,30 @@ Created on Sun Apr  8 16:54:05 2018
 from __future__ import print_function
 from keras import layers,Input
 from keras.models import Model
-from keras.applications.nasnet import NASNetLarge
+from keras.applications.nasnet import NASNetLarge, NASNetMobile
 
 
 class Network(object):
     def __init__(self,kwargs):
-        self.unfreeze_layers = kwargs.unfreeze_layers
+        pass
         
-    def get_network(self):
-        model = NASNetLarge(include_top = False, input_shape=(448,448,3))
-        x = model.output
+    def get_network(self,options):
+        if options.pretrained:
+            init_model = NASNetLarge(include_top = False, weights = 'imagenet', input_shape=(448,448,3))
+        else:
+            init_model = NASNetLarge(include_top = False, input_shape=(448,448,3))
+        
+        x = init_model.output
         x = layers.Flatten()(x)
         x = layers.Dense(1024, activation="relu")(x)
         x = layers.Dropout(0.5)(x)
         x = layers.Dense(1024, activation="relu")(x)
         predictions = layers.Dense(128, activation="softmax")(x)
-        return Model(model.input, predictions)
+        
+        if options.freeze_layers:
+            for layer in init_model.layers:
+                if any(map(layer.name.startswith, options.unfreeze_layers)):
+                    layer.trainable = True
+                else:
+                    layer.trainable = False
+        return Model(init_model.input, predictions)
