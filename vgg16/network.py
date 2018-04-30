aaf8525c9df8bb53a436435e13f878a6ae226abb
aB9 <abhi09.mu@gmail.com>
2018-04-30 15:52:17 -0700

    added classifier and unfreeze layer code

--- vgg16/network.py
+++ vgg16/network.py
@@ -16,51 +16,9 @@ class Network(object):
         else:
             model = VGG16(include_top=False, input_shape=(224, 224, 3))
 
-        input_model = model.output
-
-        # size 64
-        block1_pad1 = layers.ZeroPadding2D((1, 1), input_model)
-        block1_conv1 = layers.Conv2D(64, kernel_size=(3, 3), activation='relu')(block1_pad1)
-        block1_pad2 = layers.ZeroPadding2D((1, 1), block1_conv1)
-        block1_conv2 = layers.Conv2D(64, kernel_size=(3, 3), activation='relu')(block1_pad2)
-        block1_maxpool = layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(block1_conv2)
-
-        # size 128
-        block2_pad1 = layers.ZeroPadding2D((1, 1), block1_maxpool)
-        block2_conv1 = layers.Conv2D(128, kernel_size=(3, 3), activation='relu')(block2_pad1)
-        block2_pad2 = layers.ZeroPadding2D((1, 1), block2_conv1)
-        block2_conv2 = layers.Conv2D(128, kernel_size=(3, 3), activation='relu')(block2_pad2)
-        block2_maxpool = layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(block2_conv2)
-
-        # size 256
-        block3_pad1 = layers.ZeroPadding2D((1, 1), block2_maxpool)
-        block3_conv1 = layers.Conv2D(256, kernel_size=(3, 3), activation='relu')(block3_pad1)
-        block3_pad2 = layers.ZeroPadding2D((1, 1), block3_conv1)
-        block3_conv2 = layers.Conv2D(256, kernel_size=(3, 3), activation='relu')(block3_pad2)
-        block3_pad3 = layers.ZeroPadding2D((1, 1), block3_conv2)
-        block3_conv3 = layers.Conv2D(256, kernel_size=(3, 3), activation='relu')(block3_pad3)
-        block3_maxpool = layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(block3_conv3)
-
-        # size 512
-        block4_pad1 = layers.ZeroPadding2D((1, 1), block3_maxpool)
-        block4_conv1 = layers.Conv2D(512, kernel_size=(3, 3), activation='relu')(block4_pad1)
-        block4_pad2 = layers.ZeroPadding2D((1, 1), block4_conv1)
-        block4_conv2 = layers.Conv2D(512, kernel_size=(3, 3), activation='relu')(block4_pad2)
-        block4_pad3 = layers.ZeroPadding2D((1, 1), block4_conv2)
-        block4_conv3 = layers.Conv2D(512, kernel_size=(3, 3), activation='relu')(block4_pad3)
-        block4_maxpool = layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(block4_conv3)
-
-        # size 512
-        block5_pad1 = layers.ZeroPadding2D((1, 1), block4_maxpool)
-        block5_conv1 = layers.Conv2D(512, kernel_size=(3, 3), activation='relu')(block5_pad1)
-        block5_pad2 = layers.ZeroPadding2D((1, 1), block5_conv1)
-        block5_conv2 = layers.Conv2D(512, kernel_size=(3, 3), activation='relu')(block5_pad2)
-        block5_pad3 = layers.ZeroPadding2D((1, 1), block5_conv2)
-        block5_conv3 = layers.Conv2D(512, kernel_size=(3, 3), activation='relu')(block5_pad3)
-        block5_maxpool = layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(block5_conv3)
-
+        conv_base_model = model.output
         # Fully Connected layer
-        fc_flatten = layers.Flatten()(block5_maxpool)
+        fc_flatten = layers.Flatten()(conv_base_model)
         fc_dense1 = layers.Dense(4096, activation='relu')(fc_flatten)
         fc_dropout1 = layers.Dropout(0.5)(fc_dense1)
         fc_dense2 = layers.Dense(4096, activation='relu')(fc_dropout1)
